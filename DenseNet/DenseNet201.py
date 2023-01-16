1327672a725462b1a01cac6e0a16e350958f6d36
Fardin <60337534+FardinHash@users.noreply.github.com>
2023-01-16 17:05:27 +0600

    DenseNet201

---
+++ DenseNet/DenseNet201.py
@@ -0,0 +1,26 @@
+# import libraries
+from keras.applications import DenseNet201
+from keras.layers import Input
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+
+# define input shape
+input_shape = (224, 224, 3)
+
+# create the base model
+base_model = DenseNet201(weights = 'imagenet', 
+                      include_top = False, 
+                      input_shape = input_shape)
+
+#freeze the layers
+for layer in base_model.layers:
+    layer.trainable = False
+
+#create the new model
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation = 'relu')(x)
+x = Dense(1024, activation = 'relu')(x)
+x = Dense(512, activation = 'relu')(x)
+predictions = Dense(num_classes, activation = 'softmax')(x)
+model = Model(inputs = base_model.input, outputs = predictions)

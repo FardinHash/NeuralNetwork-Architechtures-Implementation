a34eb036ac55ef1fae163860c745eeac18b51e2d
Fardin <60337534+FardinHash@users.noreply.github.com>
2023-01-16 12:37:57 +0600

    details way

---
+++ VGG16/vgg_16.py
@@ -0,0 +1,27 @@
+# import libraries
+from keras import layers
+from keras import models
+from keras.applications import VGG16
+from keras.layers import Input
+
+#define input shape
+input_shape = (224, 224, 3)
+
+#create the base model
+base_model = VGG16(weights = 'imagenet', 
+                      include_top = False, 
+                      input_shape = input_shape)
+
+#freeze the layers
+for layer in base_model.layers:
+    layer.trainable = False
+
+#create the model
+x = base_model.output
+x = layers.GlobalAveragePooling2D()(x)
+x = layers.Dense(1024, activation = 'relu')(x)
+x = layers.Dropout(0.5)(x)
+
+# predictions
+predictions = layers.Dense(num_classes, activation = 'softmax')(x)
+model = models.Model(inputs = base_model.input, outputs = predictions)

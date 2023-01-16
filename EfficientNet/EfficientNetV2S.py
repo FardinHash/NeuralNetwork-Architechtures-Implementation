a70659d6d03bf883c904436d461a805a98b7ce86
Fardin <60337534+FardinHash@users.noreply.github.com>
2023-01-17 03:03:20 +0600

    using tensorflow

---
+++ EfficientNet/EfficientNetV2S.py
@@ -0,0 +1,19 @@
+# import libraries
+import tensorflow as tf
+
+# Load the model from TensorFlow
+model = tf.keras.Sequential([
+    tf.keras.layers.InputLayer(input_shape=(None, None, 3)),
+    tf.keras.applications.EfficientNetV2S(weights = 'imagenet', include_top = True)
+])
+
+# Compile the model
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+# Train the model on dataset
+model.fit(X_train, y_train, epochs=10, batch_size = 32)
+
+# Evaluate the model on test dataset
+test_loss, test_acc = model.evaluate(X_test, y_test)
+
+print('Test accuracy:', test_acc)

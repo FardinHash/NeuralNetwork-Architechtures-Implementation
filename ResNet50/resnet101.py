4a2711d69c80464ca0febb49a109bef65f35624d
Fardin <60337534+FardinHash@users.noreply.github.com>
2023-01-16 13:13:54 +0600

    resnet101

---
+++ ResNet50/resnet101.py
@@ -0,0 +1,24 @@
+# import libraries
+from keras.applications import ResNet101
+from keras.layers import Input
+
+# define input shape
+input_shape = (224, 224, 3)
+
+# create the base model
+base_model = ResNet101(weights = 'imagenet',
+                      include_top = False,
+                      input_shape = input_shape)
+
+# add a global average pooling layer
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+
+# add a fully-connected layer
+x = Dense(1024, activation = 'relu')(x)
+
+# add a logistic layer
+predictions = Dense(num_classes, activation = 'softmax')(x)
+
+# this is the model will be trained
+model = Model(inputs = base_model.input, outputs = predictions)

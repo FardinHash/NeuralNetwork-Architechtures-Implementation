c4bfc177ff4f34c734c8cee80390ed447a9db3ba
Fardin <60337534+FardinHash@users.noreply.github.com>
2023-01-16 12:33:20 +0600

    xception

---
+++ Xception/xception.py
@@ -0,0 +1,25 @@
+# import libraries
+from keras.applications import Xception
+from keras.layers import Input
+
+# define input shape
+input_shape = (299, 299, 3)
+
+# create the base model
+base_model = Xception(weights='imagenet',
+                      include_top=False,
+                      input_shape=input_shape)
+
+# add a global average pooling layer
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+
+# add a fully-connected layer
+x = Dense(1024, activation='relu')(x)
+
+# add a logistic layer
+predictions = Dense(num_classes, activation='softmax')(x)
+
+# this is the model will be trained
+model = Model(inputs=base_model.input, outputs=predictions)
+
